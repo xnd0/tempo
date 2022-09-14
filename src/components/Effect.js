e68d093e5af32332af2fe9a72439daf6f07883bc
xndcmd <xndcmd@gmail.com>
2022-09-14 13:05:42 -0700

    first sound successfully plays on button click

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import useSound from 'use-sound';
+
+import click1 from '../components/assets/sounds/01-C3.mp3';
 
 
 
 function Effect() {
 
+    const [playclick1] = useSound(click1);
+
     return (
         <div>
-            Effects (Metronome for now) Component Here
+
+            <div>
+                Effects (Metronome for now) Component Here
+            </div>
+
+            <div>
+                <button onClick={playclick1}>Sound!</button>
+            </div>
+
         </div>
     )
 
