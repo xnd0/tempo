5c778ceb656aea82f7ea06a37d6b4fbb3ac84364
xndcmd <xndcmd@gmail.com>
2022-09-14 12:38:17 -0700

    added starter image + sound assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Keyboard from "./components/Keyboard"
+import Effect from "./components/Effect"
 
 function App() {
   return (
@@ -8,10 +9,14 @@ function App() {
       <header className="App-header">
         Keyboard Noise
       </header>
-
+      <div>
+        <Effect />
+      </div>
       <div>
         <Keyboard />
       </div>
+
+
 
     </div>
   );
---
+++ src/components/Effect.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function Effect() {
+
+    return (
+        <div>
+            Effects (Metronome for now) Component Here
+        </div>
+    )
+
+}
+
+
+export default Effect;
