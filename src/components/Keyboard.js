53744908efbd7204adafadb367963ee3747232cb
xndcmd <xndcmd@gmail.com>
2022-09-14 10:58:06 -0700

    added and linked new keyboard component

---
+++ src/components/Keyboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Keyboard() {
+
+
+
+    return (
+        <div>
+            Keyboard Component Here
+        </div>
+    );
+
+};
+
+export default Keyboard;
